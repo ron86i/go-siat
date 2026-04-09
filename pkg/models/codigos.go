db589fdb3d8b4899d9e2856c20ba7fe0f01be2e4
ron86i <ronaldorua86@gmail.com>
2026-04-09 00:18:27 -0400

    docs: improve GoDoc for Codigos builder methods

--- pkg/models/codigos.go
+++ pkg/models/codigos.go
@@ -124,36 +124,43 @@ type verificarNitBuilder struct {
 	request *codigos.VerificarNit
 }
 
+// WithCodigoAmbiente establece el código de ambiente (Piloto o Producción).
 func (b *verificarNitBuilder) WithCodigoAmbiente(codigoAmbiente int) *verificarNitBuilder {
 	b.request.SolicitudVerificarNit.CodigoAmbiente = codigoAmbiente
 	return b
 }
 
+// WithCodigoModalidad establece el código de la modalidad de facturación.
 func (b *verificarNitBuilder) WithCodigoModalidad(codigoModalidad int) *verificarNitBuilder {
 	b.request.SolicitudVerificarNit.CodigoModalidad = codigoModalidad
 	return b
 }
 
+// WithCodigoSistema establece el código del sistema autorizado por el SIN.
 func (b *verificarNitBuilder) WithCodigoSistema(codigoSistema string) *verificarNitBuilder {
 	b.request.SolicitudVerificarNit.CodigoSistema = codigoSistema
 	return b
 }
 
+// WithCodigoSucursal establece el código de la sucursal.
 func (b *verificarNitBuilder) WithCodigoSucursal(codigoSucursal int) *verificarNitBuilder {
 	b.request.SolicitudVerificarNit.CodigoSucursal = codigoSucursal
 	return b
 }
 
+// WithCuis establece el Código Único de Inicio de Sistemas.
 func (b *verificarNitBuilder) WithCuis(cuis string) *verificarNitBuilder {
 	b.request.SolicitudVerificarNit.Cuis = cuis
 	return b
 }
 
+// WithNit establece el NIT del emisor.
 func (b *verificarNitBuilder) WithNit(nit int64) *verificarNitBuilder {
 	b.request.SolicitudVerificarNit.Nit = nit
 	return b
 }
 
+// WithNitParaVerificacion establece el NIT que se desea validar ante el SIN.
 func (b *verificarNitBuilder) WithNitParaVerificacion(nitParaVerificacion int64) *verificarNitBuilder {
 	b.request.SolicitudVerificarNit.NitParaVerificacion = nitParaVerificacion
 	return b
@@ -169,31 +176,37 @@ type cuisBuilder struct {
 	request *codigos.Cuis
 }
 
+// WithCodigoAmbiente establece el código de ambiente.
 func (b *cuisBuilder) WithCodigoAmbiente(codigoAmbiente int) *cuisBuilder {
 	b.request.SolicitudCuis.CodigoAmbiente = codigoAmbiente
 	return b
 }
 
+// WithCodigoModalidad establece el código de la modalidad.
 func (b *cuisBuilder) WithCodigoModalidad(codigoModalidad int) *cuisBuilder {
 	b.request.SolicitudCuis.CodigoModalidad = codigoModalidad
 	return b
 }
 
+// WithCodigoPuntoVenta establece el código del punto de venta.
 func (b *cuisBuilder) WithCodigoPuntoVenta(codigoPuntoVenta int) *cuisBuilder {
 	b.request.SolicitudCuis.CodigoPuntoVenta = codigoPuntoVenta
 	return b
 }
 
+// WithCodigoSucursal establece el código de la sucursal.
 func (b *cuisBuilder) WithCodigoSucursal(codigoSucursal int) *cuisBuilder {
 	b.request.SolicitudCuis.CodigoSucursal = codigoSucursal
 	return b
 }
 
+// WithCodigoSistema establece el código del sistema.
 func (b *cuisBuilder) WithCodigoSistema(codigoSistema string) *cuisBuilder {
 	b.request.SolicitudCuis.CodigoSistema = codigoSistema
 	return b
 }
 
+// WithNit establece el NIT del emisor.
 func (b *cuisBuilder) WithNit(nit int64) *cuisBuilder {
 	b.request.SolicitudCuis.Nit = nit
 	return b
@@ -209,36 +222,43 @@ type cufdBuilder struct {
 	request *codigos.Cufd
 }
 
+// WithCodigoAmbiente establece el código de ambiente.
 func (b *cufdBuilder) WithCodigoAmbiente(codigoAmbiente int) *cufdBuilder {
 	b.request.SolicitudCufd.CodigoAmbiente = codigoAmbiente
 	return b
 }
 
+// WithCodigoModalidad establece el código de la modalidad.
 func (b *cufdBuilder) WithCodigoModalidad(codigoModalidad int) *cufdBuilder {
 	b.request.SolicitudCufd.CodigoModalidad = codigoModalidad
 	return b
 }
 
+// WithCodigoPuntoVenta establece el código del punto de venta.
 func (b *cufdBuilder) WithCodigoPuntoVenta(codigoPuntoVenta int) *cufdBuilder {
 	b.request.SolicitudCufd.CodigoPuntoVenta = codigoPuntoVenta
 	return b
 }
 
+// WithCodigoSistema establece el código del sistema.
 func (b *cufdBuilder) WithCodigoSistema(codigoSistema string) *cufdBuilder {
 	b.request.SolicitudCufd.CodigoSistema = codigoSistema
 	return b
 }
 
+// WithCodigoSucursal establece el código de la sucursal.
 func (b *cufdBuilder) WithCodigoSucursal(codigoSucursal int) *cufdBuilder {
 	b.request.SolicitudCufd.CodigoSucursal = codigoSucursal
 	return b
 }
 
+// WithCuis establece el CUIS.
 func (b *cufdBuilder) WithCuis(cuis string) *cufdBuilder {
 	b.request.SolicitudCufd.Cuis = cuis
 	return b
 }
 
+// WithNit establece el NIT del emisor.
 func (b *cufdBuilder) WithNit(nit int64) *cufdBuilder {
 	b.request.SolicitudCufd.Nit = nit
 	return b
@@ -254,26 +274,31 @@ type cuisMasivoBuilder struct {
 	request *codigos.CuisMasivo
 }
 
+// WithCodigoAmbiente establece el código de ambiente.
 func (b *cuisMasivoBuilder) WithCodigoAmbiente(codigoAmbiente int) *cuisMasivoBuilder {
 	b.request.SolicitudCuisMasivoSistemas.CodigoAmbiente = codigoAmbiente
 	return b
 }
 
+// WithCodigoModalidad establece el código de la modalidad.
 func (b *cuisMasivoBuilder) WithCodigoModalidad(codigoModalidad int) *cuisMasivoBuilder {
 	b.request.SolicitudCuisMasivoSistemas.CodigoModalidad = codigoModalidad
 	return b
 }
 
+// WithCodigoSistema establece el código del sistema.
 func (b *cuisMasivoBuilder) WithCodigoSistema(codigoSistema string) *cuisMasivoBuilder {
 	b.request.SolicitudCuisMasivoSistemas.CodigoSistema = codigoSistema
 	return b
 }
 
+// WithNit establece el NIT del emisor.
 func (b *cuisMasivoBuilder) WithNit(nit int64) *cuisMasivoBuilder {
 	b.request.SolicitudCuisMasivoSistemas.Nit = nit
 	return b
 }
 
+// WithDatosSolicitud establece la lista de sucursales y puntos de venta para los cuales se solicita CUIS.
 func (b *cuisMasivoBuilder) WithDatosSolicitud(builders ...*SolicitudListaCuisDtoBuilder) *cuisMasivoBuilder {
 	datos := make([]codigos.SolicitudListaCuisDto, len(builders))
 	for i, builder := range builders {
@@ -293,26 +318,31 @@ type cufdMasivoBuilder struct {
 	request *codigos.CufdMasivo
 }
 
+// WithCodigoAmbiente establece el código de ambiente.
 func (b *cufdMasivoBuilder) WithCodigoAmbiente(codigoAmbiente int) *cufdMasivoBuilder {
 	b.request.SolicitudCufdMasivo.CodigoAmbiente = codigoAmbiente
 	return b
 }
 
+// WithCodigoModalidad establece el código de la modalidad.
 func (b *cufdMasivoBuilder) WithCodigoModalidad(codigoModalidad int) *cufdMasivoBuilder {
 	b.request.SolicitudCufdMasivo.CodigoModalidad = codigoModalidad
 	return b
 }
 
+// WithCodigoSistema establece el código del sistema.
 func (b *cufdMasivoBuilder) WithCodigoSistema(codigoSistema string) *cufdMasivoBuilder {
 	b.request.SolicitudCufdMasivo.CodigoSistema = codigoSistema
 	return b
 }
 
+// WithNit establece el NIT del emisor.
 func (b *cufdMasivoBuilder) WithNit(nit int64) *cufdMasivoBuilder {
 	b.request.SolicitudCufdMasivo.Nit = nit
 	return b
 }
 
+// WithDatosSolicitud establece la lista de sucursales, puntos de venta y CUIS para los cuales se solicita CUFD.
 func (b *cufdMasivoBuilder) WithDatosSolicitud(builders ...*SolicitudListaCufdDtoBuilder) *cufdMasivoBuilder {
 	datos := make([]codigos.SolicitudListaCufdDto, len(builders))
 	for i, builder := range builders {
@@ -332,41 +362,49 @@ type notificaCertificadoRevocadoBuilder struct {
 	request *codigos.NotificaCertificadoRevocado
 }
 
+// WithCertificado establece el certificado revocado en formato Base64.
 func (b *notificaCertificadoRevocadoBuilder) WithCertificado(certificado string) *notificaCertificadoRevocadoBuilder {
 	b.request.SolicitudNotificaRevocado.Certificado = certificado
 	return b
 }
 
+// WithCodigoAmbiente establece el código de ambiente.
 func (b *notificaCertificadoRevocadoBuilder) WithCodigoAmbiente(codigoAmbiente int) *notificaCertificadoRevocadoBuilder {
 	b.request.SolicitudNotificaRevocado.CodigoAmbiente = codigoAmbiente
 	return b
 }
 
+// WithCodigoSistema establece el código del sistema.
 func (b *notificaCertificadoRevocadoBuilder) WithCodigoSistema(codigoSistema string) *notificaCertificadoRevocadoBuilder {
 	b.request.SolicitudNotificaRevocado.CodigoSistema = codigoSistema
 	return b
 }
 
+// WithCodigoSucursal establece el código de la sucursal.
 func (b *notificaCertificadoRevocadoBuilder) WithCodigoSucursal(codigoSucursal int) *notificaCertificadoRevocadoBuilder {
 	b.request.SolicitudNotificaRevocado.CodigoSucursal = codigoSucursal
 	return b
 }
 
+// WithCuis establece el CUIS.
 func (b *notificaCertificadoRevocadoBuilder) WithCuis(cuis string) *notificaCertificadoRevocadoBuilder {
 	b.request.SolicitudNotificaRevocado.Cuis = cuis
 	return b
 }
 
+// WithNit establece el NIT del emisor.
 func (b *notificaCertificadoRevocadoBuilder) WithNit(nit int64) *notificaCertificadoRevocadoBuilder {
 	b.request.SolicitudNotificaRevocado.Nit = nit
 	return b
 }
 
+// WithRazonRevocacion establece el motivo de la revocación del certificado.
 func (b *notificaCertificadoRevocadoBuilder) WithRazonRevocacion(razonRevocacion string) *notificaCertificadoRevocadoBuilder {
 	b.request.SolicitudNotificaRevocado.RazonRevocacion = razonRevocacion
 	return b
 }
 
+// WithFechaRevocacion establece la fecha y hora de la revocación.
 func (b *notificaCertificadoRevocadoBuilder) WithFechaRevocacion(fechaRevocacion *time.Time) *notificaCertificadoRevocadoBuilder {
 	b.request.SolicitudNotificaRevocado.FechaRevocacion = fechaRevocacion
 	return b
@@ -382,11 +420,13 @@ type SolicitudListaCuisDtoBuilder struct {
 	request *codigos.SolicitudListaCuisDto
 }
 
+// WithCodigoPuntoVenta establece el código del punto de venta para la solicitud masiva.
 func (b *SolicitudListaCuisDtoBuilder) WithCodigoPuntoVenta(codigoPuntoVenta int) *SolicitudListaCuisDtoBuilder {
 	b.request.CodigoPuntoVenta = &codigoPuntoVenta
 	return b
 }
 
+// WithCodigoSucursal establece el código de la sucursal para la solicitud masiva.
 func (b *SolicitudListaCuisDtoBuilder) WithCodigoSucursal(codigoSucursal int) *SolicitudListaCuisDtoBuilder {
 	b.request.CodigoSucursal = codigoSucursal
 	return b
@@ -401,16 +441,19 @@ type SolicitudListaCufdDtoBuilder struct {
 	request *codigos.SolicitudListaCufdDto
 }
 
+// WithCodigoPuntoVenta establece el código del punto de venta.
 func (b *SolicitudListaCufdDtoBuilder) WithCodigoPuntoVenta(codigoPuntoVenta int) *SolicitudListaCufdDtoBuilder {
 	b.request.CodigoPuntoVenta = codigoPuntoVenta
 	return b
 }
 
+// WithCodigoSucursal establece el código de la sucursal.
 func (b *SolicitudListaCufdDtoBuilder) WithCodigoSucursal(codigoSucursal int) *SolicitudListaCufdDtoBuilder {
 	b.request.CodigoSucursal = codigoSucursal
 	return b
 }
 
+// WithCuis establece el CUIS asociado a la sucursal/punto de venta.
 func (b *SolicitudListaCufdDtoBuilder) WithCuis(cuis string) *SolicitudListaCufdDtoBuilder {
 	b.request.Cuis = cuis
 	return b
